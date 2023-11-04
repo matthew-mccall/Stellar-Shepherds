18e818f02e43076971844eb75b8d31137a4654dc
Andrew <andrew@thenazareths.net>
2023-11-04 17:20:12 -0400

    Added methods for Planet Variable

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,29 +4,52 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public double temperature_change = 0;
-    public double average_carbon_units = 0;
-    public double delta_sea_level = 0;
+    public double temperatureChange = 0;
+    public double averageCarbonUnits = 0;
+    public double deltaSeaLevel = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        temperatureChange = 0;
+        averageCarbonUnits = 0;
+        deltaSeaLevel = 0;
     }
 
-    public void carbon_change(double amount)
+    public void carbonChange(double amount)
     {
-        average_carbon_units += amount;
+        averageCarbonUnits += amount;
+        return;
+    }
+
+    public double getCarbonChange()
+    {
+        return averageCarbonUnits;
+    }
+
+    public double getTemperatureChange()
+    {
+        return temperatureChange;
+    }
+
+    private void calculateTemperatureChange()
+    {
+        temperatureChange = averageCarbonUnits / 1E6;
+    }
+
+    private void calculateNewSeaLevel()
+    {
+        deltaSeaLevel = averageCarbonUnits / 1E6;
     }
     
     // Update is called once per frame
     void Update()
     {
-        
+        calculateNewSeaLevel();
     }
     
     void LateUpdate()
     {
-        
+        calculateTemperatureChange();
     }
 }
