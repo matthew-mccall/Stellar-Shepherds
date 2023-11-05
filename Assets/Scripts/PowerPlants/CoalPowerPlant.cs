b3cf83c7fbb96864e2e7bb4a393077596ab53566
Shamik Chatterjee <81479985+shamikc-dev@users.noreply.github.com>
2023-11-04 21:15:34 -0400

    Coal and nuclear power plants

---
+++ Assets/Scripts/PowerPlants/CoalPowerPlant.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoalPowerPlant : PowerPlant
+{
+
+    new public double maxPower = 100;
+    new public double carbonFootprintIntensity = 100;
+    new public double carbonFootprintRadius = 50;
+    new public double maintenanceCost = 10;
+    new public double initialCost = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PowerPlants/NuclearPowerPlant.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuclearPowerPlant : PowerPlant
+{
+    new public double maxPower = 100;
+    new public double carbonFootprintIntensity = 10;
+    new public double carbonFootprintRadius = 5;
+    new public double maintenanceCost = 60;
+    new public double initialCost = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
