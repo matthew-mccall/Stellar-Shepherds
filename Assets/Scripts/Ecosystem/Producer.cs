075a9398e9a1e0b3d6580b540f53ae248f088ca7
Andrew <andrew@thenazareths.net>
2023-11-04 20:54:57 -0400

    Updated producer growth so they rely on temp and rainfall as well

--- Assets/Scripts/Ecosystem/Producer.cs
+++ Assets/Scripts/Ecosystem/Producer.cs
@@ -5,23 +5,105 @@ using UnityEngine;
 public class Producer : Species
 {
     private List<Prey> preyList;
-
-    public double growthFactor;
-
-    public int dx;
-
-    public int dy;
-
-    public Color color;
+    public static double minRainfall;
+    public static double maxRainfall;
     // Start is called before the first frame update
     void Start()
     {
-        GenerateDensityMap(dx, dy, color);
+        GenerateDensityMap();
+    }
+    
+    public Texture2D growth(Texture2D temperatureMap, Texture2D rainfallMap)
+    {
+        Texture2D dMap = densityMap;
+        Color color = densityMap.GetPixel(0, 0);
+        for (int x = 0; x < densityMap.width; x++)
+        {
+            for (int y = 0; y < densityMap.height; y++)
+            {
+                if (rainfallMap.GetPixel(x, y).grayscale>(minRainfall/400f)&&rainfallMap.GetPixel(x, y).grayscale<(maxRainfall/400f)&&
+                    temperatureMap.GetPixel(x, y).grayscale>(minTolerantTemp/35f)&&temperatureMap.GetPixel(x, y).grayscale<(maxTolerantTemp/35f))
+                {
+                    int count = 0;
+                    float val = 0;
+                    if (y > 0)
+                    {
+                        val += densityMap.GetPixel(x, y - 1).grayscale;
+                        count++;
+                    }
+                    if (y < densityMap.height)
+                    {
+                        val += densityMap.GetPixel(x, y + 1).grayscale;
+                        count++;
+                    }
+                    if (x > 0)
+                    {
+                        val += densityMap.GetPixel(x - 1 , y ).grayscale;
+                        count++;
+                    }
+                    if (x < densityMap.width)
+                    {
+                        val += densityMap.GetPixel(x + 1, y ).grayscale;
+                        count++;
+                    }
+                    Color newColor = new Color(color.r, color.g, color.b, growthFactor * (val / count));
+                    dMap.SetPixel(x,y, newColor);
+                }
+                else
+                {
+                    Color newColor = new Color(color.r, color.g, color.b, 0);
+                    dMap.SetPixel(x,y, newColor);
+                }
+                
+            }
+        }
+        dMap.Apply();
+        return dMap;
+    }
+    
+    public Texture2D death(Texture2D predatorMap)
+    {
+        Texture2D dMap = densityMap;
+        Color color = densityMap.GetPixel(0, 0);
+        for (int x = 0; x < densityMap.width; x++)
+        {
+            for (int y = 0; y < densityMap.height; y++)
+            {
+                int count = 0;
+                float val = 0;
+                if (y > 0)
+                {
+                    val += densityMap.GetPixel(x, y - 1).grayscale;
+                    count++;
+                }
+
+                if (y < densityMap.height)
+                {
+                    val += densityMap.GetPixel(x, y + 1).grayscale;
+                    count++;
+                }
+                if (x > 0)
+                {
+                    val += densityMap.GetPixel(x - 1 , y ).grayscale;
+                    count++;
+                }
+
+                if (x < densityMap.width)
+                {
+                    val += densityMap.GetPixel(x + 1, y ).grayscale;
+                    count++;
+                }
+
+                Color newColor = new Color(color.r, color.g, color.b, deathFactor * (val / count));
+                dMap.SetPixel(x,y, newColor);
+            }
+        }
+        return dMap;
     }
     
     // Update is called once per frame
     void Update()
     {
-        
+        Texture2D growthMap = growth(planet.GetComponent<Temperature>().temperatureMap, planet.GetComponent<Rainfall>().rainfallMap);
     }
 }
--- Assets/Scripts/Ecosystem/Species.cs
+++ Assets/Scripts/Ecosystem/Species.cs
@@ -5,111 +5,38 @@ using UnityEngine.UI;
 
 public class Species : MonoBehaviour
 {
+    // Temperature is in C, Rainfall is in cm
     public static string name;
     public static double netCarbonEffect;
     public static double minTolerantTemp;
     public static double maxTolerantTemp;
     
+    public static float growthFactor;
+    public static float deathFactor;
+    public static int width;
+    public static int height;
+    public static float densityScale;
+    public static Color color;
+    
     protected Texture2D densityMap;
+    
+    public static GameObject planet;
 
-    public Planet planet;
-
-    public void GenerateDensityMap(int width, int height, Color color)
+    public void GenerateDensityMap()
     {
         densityMap = new Texture2D(width, height);
-            float scale = 0.1f;  // Adjust the scale to control the noise pattern.
-                for (int x = 0; x < densityMap.width; x++)
-            {
-                for (int y = 0; y < densityMap.height; y++)
-                { 
-                    float xCoord = (float)x / densityMap.width * scale;
-                    float yCoord = (float)y / densityMap.height * scale; 
+        for (int x = 0; x < densityMap.width; x++)
+        {
+            for (int y = 0; y < densityMap.height; y++) 
+            { 
+                    float xCoord = (float)x / densityMap.width * densityScale;
+                    float yCoord = (float)y / densityMap.height * densityScale; 
                     float density = Mathf.PerlinNoise(xCoord, yCoord);
                     Color pixelColor = new Color(color.r, color.g, color.b, density);
                     densityMap.SetPixel(x, y, pixelColor);
-                }
-            }
-            densityMap.Apply();
-    }
-
-    public Texture2D growth(Texture2D densityMap, float growthFactor)
-    {
-        Texture2D dMap = densityMap;
-        Color color = densityMap.GetPixel(0, 0);
-        for (int x = 0; x < densityMap.width; x++)
-        {
-            for (int y = 0; y < densityMap.height; y++)
-            {
-                int count = 0;
-                float val = 0;
-                if (y > 0)
-                {
-                    val += densityMap.GetPixel(x, y - 1).grayscale;
-                    count++;
-                }
-
-                if (y < densityMap.height)
-                {
-                    val += densityMap.GetPixel(x, y + 1).grayscale;
-                    count++;
-                }
-                if (x > 0)
-                {
-                    val += densityMap.GetPixel(x - 1 , y ).grayscale;
-                    count++;
-                }
-
-                if (x < densityMap.width)
-                {
-                    val += densityMap.GetPixel(x + 1, y ).grayscale;
-                    count++;
-                }
-
-                Color newColor = new Color(color.r, color.g, color.b, growthFactor * (val / count));
-                dMap.SetPixel(x,y, newColor);
             }
         }
-        return dMap;
-    }
-
-    public Texture2D death(Texture2D densityMap, Texture2D predatorMap, float deathRate)
-    {
-        Texture2D dMap = densityMap;
-        Color color = densityMap.GetPixel(0, 0);
-        for (int x = 0; x < densityMap.width; x++)
-        {
-            for (int y = 0; y < densityMap.height; y++)
-            {
-                int count = 0;
-                float val = 0;
-                if (y > 0)
-                {
-                    val += densityMap.GetPixel(x, y - 1).grayscale;
-                    count++;
-                }
-
-                if (y < densityMap.height)
-                {
-                    val += densityMap.GetPixel(x, y + 1).grayscale;
-                    count++;
-                }
-                if (x > 0)
-                {
-                    val += densityMap.GetPixel(x - 1 , y ).grayscale;
-                    count++;
-                }
-
-                if (x < densityMap.width)
-                {
-                    val += densityMap.GetPixel(x + 1, y ).grayscale;
-                    count++;
-                }
-
-                Color newColor = new Color(color.r, color.g, color.b, deathRate * (val / count));
-                dMap.SetPixel(x,y, newColor);
-            }
-        }
-        return dMap;
+        densityMap.Apply();
     }
     
     // Start is called before the first frame update
